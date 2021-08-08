95809515a4dc1a1351ae25faa6240bbea6ec40a9
Jakub <jkbsz@users.noreply.github.com>
2021-08-08 07:41:52 +0200

    Update bookmarklets.js

--- other/bookmarklets.js
+++ other/bookmarklets.js
@@ -1,5 +1,2 @@
-// Temporary notepad
-data:text/html;charset=utf-8, <title>Notepad</title><textarea style="font-size: 1.2em; width: 100%; height: 100%; border: 10px; outline: line grey; padding: 10px;" autofocus />
-
 // Insert text in the active field
 javascript:(function(a){a.value=a.value.slice(0,a.selectionStart) + "abc" + a.value.slice(a.selectionEnd);})(document.activeElement);
